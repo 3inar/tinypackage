1143d1c973b2548f7ca7310f4fd1c3446f66ff12
Einar Holsbø <einar@cs.uit.no>
2016-11-17 13:05:33 +0100

    document and export learn function

--- R/learn.R
+++ R/learn.R
@@ -1,3 +1,10 @@
+#' Learning function
+#'
+#' Tells you who is learning
+#'
+#' @param name name of person who is learning
+#'
+#' @export
 learn <- function(name) {
   print(paste0(name, " is learning to package"))
 }
